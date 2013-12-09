88bf72f1c11941b3573595959b1e68603063d308
xiechao06 <xiechao06@gmail.com>
2013-12-09 11:28:12 +0800

    fix #21

--- genuine_ap/user/views.py
+++ genuine_ap/user/views.py
@@ -6,17 +6,23 @@ from genuine_ap.user import user_page
 from genuine_ap.models import User, SPU, Favor
 from genuine_ap import utils
 
+
 @user_page.route('/favor/<spu_id>', methods=['GET', 'POST'])
 def favor(spu_id):
     user_id = request.args['user_id']
-    user = User.query.filter(User.id==user_id).one()
+    user = User.query.filter(User.id == user_id).one()
     spu = SPU.query.get_or_404(spu_id)
+    favor = Favor.query.filter(and_(Favor.spu_id == spu_id,
+                                    Favor.user_id == user_id)).first()
     if request.method == 'POST':
+        if favor:
+            return '您已经收藏了该产品!', 403
         favor = utils.do_commit(Favor(spu=spu, user=user))
-        return str(favor.id)
+        return jsonify({
+            'id': favor.id,
+            'create_time': favor.create_time.strftime('%Y-%m-%d')
+        })
     else:
-        favor = Favor.query.filter(and_(Favor.spu_id==spu_id, 
-            Favor.user_id==user_id)).first()
         if favor is None:
             return "", 404
         return jsonify({
