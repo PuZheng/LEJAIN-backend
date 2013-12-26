24d4b98142b6e58edf1136eb8260458ef7bcac67
xiechao06 <xiechao06@gmail.com>
2013-12-26 15:25:35 +0800

    add favored information when get spu and make verification

--- genuine_ap/tag/__init__.py
+++ genuine_ap/tag/__init__.py
@@ -5,3 +5,5 @@ tag_ws = Blueprint("tag_ws", __name__, static_folder="static",
                    template_folder="templates")
 
 import genuine_ap.tag.views
+from genuine_ap.apis.user import load_user_from_token
+tag_ws.before_request(load_user_from_token)
